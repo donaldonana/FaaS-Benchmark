671bd999e75f41cbfe60873eda9e79f8fa5208be
donaldonana <onanadonald@gmail.com>
2024-10-11 14:51:24 +0200

    text to speech code review.

--- Text-To-Speech/S1.py
+++ Text-To-Speech/S1.py
@@ -4,41 +4,17 @@ import subprocess
 import signal
 
 
-SPEECH_FILE     = "temp/speech.json"
-PROFANITY_FILE  = "temp/profanity.json"
-
-def cordinatoor(ipv4):
-
-    processes = []
-    speech = [
-            "wsk", "action", "invoke", "S2", "-r", "--blocking",
-            "--param", "ipv4", ipv4, "--param", "schema", "S1"
-            ]
-    profanity = [
-        "wsk", "action", "invoke", "profanity", "--result",
-        "--param", "ipv4", ipv4
-    ] 
-    processes.append(subprocess.Popen(speech,  stdout=open(SPEECH_FILE, 'w')))
-    processes.append(subprocess.Popen(profanity,  stdout=open(PROFANITY_FILE, 'w')))
-
-    for process in processes:
-        process.wait()
-    
-    return {"message" : "Ok"}
-
-
 def main():
     ipv4 = sys.argv[1]
 
     speech = [
             "wsk", "action", "invoke", "censor", "-r", "--blocking",
-            "--param", "ipv4", ipv4, "-P", SPEECH_FILE, "-P", PROFANITY_FILE
+            "--param", "ipv4", ipv4 
             ]
      
     for i in range(2):
 
         energy_process = subprocess.Popen(["cpu-energy-meter", "-r"], stdout=open("result/energy/S2/energy.txt", 'a'))
-        cordinatoor(ipv4)
         whiskprocess   = subprocess.Popen(speech)
         whiskprocess.wait()
         os.kill(energy_process.pid, signal.SIGINT)
--- Text-To-Speech/coordinator/__main__.py
+++ Text-To-Speech/coordinator/__main__.py
@@ -2,29 +2,26 @@ import requests
 from multiprocessing import Process, Manager, Lock
 
 
-def start(action, result, lock):
-    
-    params = {"ipv4" : "130.190.118.188"}
+def start(action, args, result, lock):
 
-    r = requests.get(f"http://172.17.0.1:3233/api/v1/web/guest/demo/{action}?blocking=true&result=true", 
+    apihost = f"http://172.17.0.1:3233/api/v1/web/guest/demo/{action}?blocking=true&result=true"
+    
+    r = requests.get(apihost, 
                       headers={"Content-Type": "application/json"}, 
-                      params=params)
+                      params=args)
 
     with lock:
         result.update(r.json())  
 
      
 def main(args):
-
-    first  = args.get("first")
-    second = args.get("second")
-
+ 
     manager = Manager()
     result = manager.dict()  # Shared dict
     lock = Lock()
 
-    p1 = Process(target=start, args=(first,result, lock))
-    p2 = Process(target=start, args=(second,result, lock))
+    p1 = Process(target=start, args=("S2", args, result, lock))
+    p2 = Process(target=start, args=("profanity", args, result, lock))
 
     p1.start()
     p2.start()
