c44fb8f357edf3f89156a0ef6221f6d42d475354
donaldonana <onanadonald@gmail.com>
2024-10-12 08:28:47 +0200

    code review

--- Text-To-Speech/S1.sh
+++ Text-To-Speech/S1.sh
@@ -10,15 +10,16 @@ fi
 
 IPV4=$1
  
-mkdir -p "result/energy/S2/" 
+mkdir -p "result/energy/S1/" 
  
 for (( i = 1; i <= 10; i++ )); do
     # Launch cpu-energy-meter in background and save her PID
-    cpu-energy-meter -r >> "result/energy/S2/energy.txt" &
+    cpu-energy-meter -r >> "result/energy/S1/energy.txt" &
     METER_PID=$!
 
-    wsk action invoke demo/S2 -r \
-      --param ipv4 "$IPV4" >>  "result/result.txt"
+    wsk action invoke demo/text2speech -r \
+      --param ipv4 "$IPV4" \
+      --param schema "S1" >>  "result/result.txt"
 
     kill -SIGINT $METER_PID
 
