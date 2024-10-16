012872fefe152cd0c64110fb6867a2a31d4c6015
donaldonana <onanadonald@gmail.com>
2024-10-17 00:22:29 +0200

    thumbnail code review

--- 210.thumbnaile/run.sh
+++ 210.thumbnaile/run.sh
@@ -11,6 +11,26 @@ fi
 IPV4=$1
 
 IMAGE=$2
+ 
+build="pull"
+
+if [ "$build" != "push" ] && [ "$build" != "pull" ]; then
+
+    echo "Invalid build argument. Expected 'push' or 'pull'."
+    exit 1   
+fi
+
+if [ "$build" == "push" ]; then
+    docker build -t action-python-v3.9:thumb .
+    docker tag action-python-v3.9:thumb onanad/action-python-v3.9:thumb
+    docker push onanad/action-python-v3.9:thumb
+else
+    docker pull onanad/action-python-v3.9:thumb
+fi
+
+wsk action update thumb --docker onanad/action-python-v3.9:thumb __main__.py --web true
+
+wsk action invoke thumb --result --param bib pillow --param file 500b.JPEG --param ipv4 "$ipv4"
 
 # Library List
 LIBRARY=("pillow" "wand" "pygame" "opencv")
