9b19b6c6179588de999904efb8ca63bc7305b469
donaldonana <onanadonald@gmail.com>
2024-11-05 21:17:19 +0100

    thumbnail review code

--- thumbnaile/run.sh
+++ thumbnaile/run.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
+if [ "$#" -ne 4 ]; then
   echo "Usage: $0 <ipv4> <image> <run>"
   echo "ipv4 : ipv4 for swift connection"
-  echo "prewarm :  prewarm"
+  echo "image :  image"
   exit 1
 fi
 
 IPV4=$1
 RUN=$2
-BUILD="pull"
+UPDATE=$3
+IMAGE=$4
 
-wsk package update demo
-
-# if [ "$BUILD" == "push" ]; then
-#   echo "comming son"
-# else
-#   docker pull onanad/action-python-v3.9:thumb
-#   wsk action update thumb --docker onanad/action-python-v3.9:thumb __main__.py  
-# fi
+if [ "$UPDATE" == "1" ]; then
+  docker pull onanad/action-python-v3.9:thumb
+  wsk action update thumb --docker onanad/action-python-v3.9:thumb __main__.py  
+fi
 
 # Prewarm the container
 wsk action invoke thumb --result \
@@ -26,33 +23,36 @@ wsk action invoke thumb --result \
   --param file 1Mb.JPEG \
   --param bib "pillow"
 
+
 # Run the experiment
 if [ "$RUN" == "1" ]; then
-  LIBRARY=("pillow" "wand" "pygame" "opencv")
-  RESULT_FILE="result/result.txt"
-  ENERGY_DIR="result/energy"
 
-  mkdir -p "$ENERGY_DIR/$IMAGE" 
-  
+  LIBRARY=("pillow" "wand" "pygame" "opencv")
+  mkdir -p "result/energy/$IMAGE" 
+
   for LIB in "${LIBRARY[@]}"; do
     echo -e "$LIB"  
-    ENERGY_FILE="$ENERGY_DIR/$IMAGE/$LIB$IMAGE.txt"  
+    ENERGY_FILE="result/energy/$IMAGE/$LIB$IMAGE.txt" 
+
     for (( i = 1; i <= 10; i++ )); do
       # Launch cpu-energy-meter in background and save her PID
       cpu-energy-meter -r >> $ENERGY_FILE &
       METER_PID=$!
       wsk action invoke thumb -r \
-        --param bib "$LIB" \
+        --param bib  "$LIB" \
         --param ipv4 "$IPV4" \
-        --param file "$IMAGE" >> $RESULT_FILE
+        --param file "$IMAGE" >> result/result.txt
 
       kill -SIGINT $METER_PID
       echo -e "$i"
 
       sleep 2
     done
-      
+        
   done
 
 fi
+
+
+
 
