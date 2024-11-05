22195b31e88c8d962d52e9b68c240ef66789b57a
donaldonana <onanadonald@gmail.com>
2024-11-05 22:28:05 +0100

    video processing code review

--- video-processing/run.sh
+++ video-processing/run.sh
@@ -1,54 +1,53 @@
-
 #!/bin/bash
-if [ "$#" -ne 3 ]; then
-  echo "Usage: $0 <ipv4> <VIDEO> <run>"
+
+if [ "$#" -ne 4 ]; then
+  echo "Usage: $0 <ipv4> <image> <run>"
   echo "ipv4 : ipv4 for swift connection"
-  echo "video : video "
-  echo "run :  run "
+  echo "run  :  run"
+  echo "update :  update"
+  echo "image  :  image"
   exit 1
 fi
+
 IPV4=$1
-VIDEO=$2
-RUN=$3
-BUILD="pull"
+RUN=$2
+UPDATE=$3
+VIDEO=$4
 
-if [ "$BUILD" != "push" ] && [ "$BUILD" != "pull" ]; then
-  echo "Invalid build argument. Expected 'push' or 'pull'."
-  exit 1   
-fi
 
-if [ "$BUILD" == "push" ]; then
-  echo "comming son"
-else
-  docker pull onanad/action-python-v3.9:proc
-  wsk action update proc --docker onanad/action-python-v3.9:proc __main__.py -t 125000 -m 1024
+if [ "$UPDATE" == "1" ]; then
+  docker pull onanad/action-python-v3.9:thumb
+  wsk action update thumb --docker onanad/action-python-v3.9:thumb __main__.py  
 fi
 
 # Prewarm the container
-wsk action invoke proc --result --param bib moviepy --param file 1Mb.avi --param ipv4 "$IPV4"
-
+wsk action invoke thumb --result \
+  --param ipv4 "$IPV4" \
+  --param file 1Mb.avi \
+  --param bib "moviepy"
+ 
 # Run the experiment
 if [ "$RUN" == "1" ]; then
-  LIBRARY=("moviepy" "ffmpeg" "imageio" "opencv")
-  RESULT_FILE="result/result.txt"
-  ENERGY_DIR="result/energy"
 
-  mkdir -p "$ENERGY_DIR/$VIDEO" 
-  
+  LIBRARY=("moviepy" "ffmpeg" "imageio" "opencv")
+  mkdir -p "result/energy/$VIDEO"
+
   for LIB in "${LIBRARY[@]}"; do
     echo -e "$LIB"  
-    ENERGY_FILE="$ENERGY_DIR/$VIDEO/$LIB$VIDEO.txt"  
+    ENERGY_FILE="result/energy/$VIDEO/$LIB$VIDEO.txt"  
       
     for (( i = 1; i <= 10; i++ )); do
       # Launch cpu-energy-meter in background and save her PID
       cpu-energy-meter -r >> $ENERGY_FILE &
       METER_PID=$!
       wsk action invoke proc -r \
-        --param bib "$LIB" \
+        --param bib  "$LIB" \
         --param ipv4 "$IPV4" \
-        --param file "$VIDEO" >> $RESULT_FILE
+        --param file "$VIDEO" >> result/result.txt
+
       kill -SIGINT $METER_PID
       echo -e "$i"
+      
       sleep 2
     done
       
