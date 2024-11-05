135b4fbfb7712d6333676f7ff6a40e7279a228d4
donaldonana <onanadonald@gmail.com>
2024-11-05 21:55:16 +0100

    thumbnail review code

--- thumbnaile/run.sh
+++ thumbnaile/run.sh
@@ -3,6 +3,8 @@
 if [ "$#" -ne 4 ]; then
   echo "Usage: $0 <ipv4> <image> <run>"
   echo "ipv4 : ipv4 for swift connection"
+  echo "run :  run"
+  echo "update :  update"
   echo "image :  image"
   exit 1
 fi
@@ -23,7 +25,6 @@ wsk action invoke thumb --result \
   --param file 1Mb.JPEG \
   --param bib "pillow"
 
-
 # Run the experiment
 if [ "$RUN" == "1" ]; then
 
@@ -31,24 +32,24 @@ if [ "$RUN" == "1" ]; then
   mkdir -p "result/energy/$IMAGE" 
 
   for LIB in "${LIBRARY[@]}"; do
-    echo -e "$LIB"  
-    ENERGY_FILE="result/energy/$IMAGE/$LIB$IMAGE.txt" 
+    echo -e "$LIB"
+    ENERGY_FILE="result/energy/$IMAGE/$LIB$IMAGE.txt"
 
     for (( i = 1; i <= 10; i++ )); do
-      # Launch cpu-energy-meter in background and save her PID
-      cpu-energy-meter -r >> $ENERGY_FILE &
+      # Launch cpu-energy-meter in background and save its PID
+      cpu-energy-meter -r >> "$ENERGY_FILE" &
       METER_PID=$!
       wsk action invoke thumb -r \
         --param bib  "$LIB" \
         --param ipv4 "$IPV4" \
         --param file "$IMAGE" >> result/result.txt
 
-      kill -SIGINT $METER_PID
+      kill -SIGINT "$METER_PID"
       echo -e "$i"
 
       sleep 2
     done
-        
+
   done
 
 fi
