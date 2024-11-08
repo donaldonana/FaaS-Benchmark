e05f6f9f9b7d1d301f4df86dd8e26b4b5cb5a861
donaldonana <onanadonald@gmail.com>
2024-11-08 11:02:54 +0100

    coede review for face recognition

--- facial-recognition/run.py
+++ facial-recognition/run.py
@@ -5,28 +5,29 @@ import signal
 
 IPV4 = "172.20.10.2"
 SCHEMA = "S1"
-VIDEO = "daenerys.mp4"
-DURATION = 35
 PROCESS = 10
+DURATION = 35
+VIDEO = "daenerys.mp4"
 ENERGY_FILE = f"result/energy/{VIDEO}.txt"
-
 os.makedirs(f"result/energy/{SCHEMA}", exist_ok=True)
+
  
 for i in range(1, 2):
+
     energy_process = subprocess.Popen(["cpu-energy-meter", "-r"], stdout=open(ENERGY_FILE, 'a'))
-    try:
-        whiskprocess = subprocess.Popen(["python3", 
+    
+    whiskprocess = subprocess.Popen(["python3", 
         f"schema/{SCHEMA}.py", 
         IPV4, str(PROCESS), 
         str(DURATION), 
         SCHEMA, 
         VIDEO, 
         str(i)], 
-        stdout=open("result/result.txt", 'a'))
+        stdout=open("result/result.txt", 'a')
+    )
 
-    finally:
-        whiskprocess.wait()
-        os.kill(energy_process.pid, signal.SIGINT)
+    whiskprocess.wait()
+    os.kill(energy_process.pid, signal.SIGINT)
 
     print(f"{i}")
 
