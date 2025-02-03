87db703c5f35218b890a9b6d7345a30666253763
donaldonana <onanadonald@gmail.com>
2025-02-03 20:36:34 +0100

    text to speech

--- text-to-speech/run.sh
+++ text-to-speech/run.sh
@@ -1,39 +1,43 @@
 #!/bin/bash
 
-
 IPV4="172.20.20.77"
 SCHEMA="S5"
 TEXTES=("1Ko.txt" "5Ko.txt" "12Ko.txt")
- 
  
 mkdir -p "result/energy/$SCHEMA/" 
 
 for TEXT in "${TEXTES[@]}"; do
 
-  echo "$TEXT" 
-  #echo -e "$TEXT" >> perfEnergy.txt
-
-  for (( i = 1; i <= 1 ; i++ )); do
+    echo "$TEXT" 
+    #echo -e "$TEXT" >> perfEnergy.txt
     
-    # Launch cpu-energy-meter in background and save her PID
-    cpu-energy-meter -r >> "result/energy/$SCHEMA/$TEXT" &
-    METER_PID=$!
+    for (( i = 1; i <= 1 ; i++ )); do
+    
+        # Launch cpu-energy-meter in background and save her PID
+        cpu-energy-meter -r >> "result/energy/$SCHEMA/$TEXT" &
+        METER_PID=$!
 
-    wsk action invoke  "demo/$SCHEMA" -r \
-        --param ipv4   "$IPV4" \
-        --param schema "$SCHEMA" \
-        --param text   "$TEXT" 
+        wsk action invoke  "demo/$SCHEMA" -r \
+            --param ipv4   "$IPV4" \
+            --param schema "$SCHEMA" \
+            --param text   "$TEXT" 
         
-    kill -SIGINT $METER_PID
+        kill -SIGINT $METER_PID
 
-    #perf stat -o result/perfEnergyS1.txt --append -e power/energy-pkg/ -e power/energy-cores/ wsk action invoke demo/text2speech -r --param ipv4 "$IPV4"   --param schema "S1" --param text "$TEXT" >> "result/perf.txt"
+        # Energy capturing with perf
 
-      echo -e "$i"
-      
-      sleep 2
-      
+        # perf stat -o result/perfEnergyS1.txt \
+        #     --append -e power/energy-pkg/ -e power/energy-cores/ wsk action invoke demo/text2speech -r \
+        #     --param ipv4 "$IPV4" \
+        #     --param schema "S1" \
+        #     --param text "$TEXT" >> "result/perf.txt"
+
+        echo -e "$i"
+        
+        sleep 2
+        
     done
 
-  done
+done
 
  
