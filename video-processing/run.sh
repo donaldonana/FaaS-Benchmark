14a5951445912ee343a25a08741763cb9c85b795
donaldonana <onanadonald@gmail.com>
2025-02-21 14:14:29 +0100

    structuration

--- video-processing/run.sh
+++ video-processing/run.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
  
-
 VIDEO="1Mb.avi"
-IPV4="172.20.20.77"
+IPV4="172.20.20.78"
 LIBRARY=("moviepy" "ffmpeg" "imageio" "opencv")
  
-mkdir -p "result/energy/$VIDEO"
+mkdir -p "result1/energy/$VIDEO"
 
 for LIB in "${LIBRARY[@]}"; do
 
   echo -e "$LIB"
-  ENERGY_FILE="result/energy/$VIDEO/$LIB$VIDEO.txt"  
+  ENERGY_FILE="result1/energy/$VIDEO/$LIB$VIDEO.txt"  
 
   for (( i = 1; i <= 10; i++ )); do
 
@@ -21,7 +20,7 @@ for LIB in "${LIBRARY[@]}"; do
     wsk action invoke proc -r \
         --param bib  "$LIB" \
         --param ipv4 "$IPV4" \
-        --param file "$VIDEO" >> result/result.txt
+        --param file "$VIDEO" >> result1/result.txt
 
     kill -SIGINT "$METER_PID"
 
