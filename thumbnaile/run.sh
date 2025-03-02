1401ff560cdd4b58477ca628c9d3f717d05cc475
donaldonana <onanadonald@gmail.com>
2025-03-02 15:16:36 +0100

    restructuration

--- thumbnaile/run.sh
+++ thumbnaile/run.sh
@@ -1,33 +1,37 @@
 #!/bin/bash
 
-IMAGE="1Mb.JPEG"
 IPV4="172.20.20.77"
+IMAGES=("100Kb.JPEG" "500b.JPEG" "1Mb.JPEG" "15Mb.JPEG" "256Kb.JPEG")
 LIBRARY=("pillow" "wand" "pygame" "opencv")
 
-mkdir -p "result/energy/energy/$IMAGE" 
 
-for LIB in "${LIBRARY[@]}"; do
+for IMAGE in "${IMAGES[@]}"; do
+  echo -e "$IMAGE"
+  mkdir -p "result/results/energy/$IMAGE" 
 
-  echo -e "$LIB"
-  ENERGY_FILE="result/energy//energy/$IMAGE/$LIB$IMAGE.txt"  
+  for LIB in "${LIBRARY[@]}"; do
+    echo -e "$LIB"
+    ENERGY_FILE="result/results/energy/$IMAGE/$LIB$IMAGE.txt"  
 
-  for (( i = 1; i <= 100; i++ )); do
- 
-    # Launch cpu-energy-meter in background and save its PID
-    cpu-energy-meter -r >> "$ENERGY_FILE" &
-    METER_PID=$!
-      
-    wsk action invoke thumb -r \
-      --param bib  "$LIB" \
-      --param ipv4 "$IPV4" \
-      --param file "$IMAGE" >> result/energy/result.txt
+    for (( i = 1; i <= 100; i++ )); do
+      # Launch cpu-energy-meter in background and save its PID
+      cpu-energy-meter -r >> "$ENERGY_FILE" &
+      METER_PID=$!
+        
+      wsk action invoke thumb -r \
+        --param bib  "$LIB" \
+        --param ipv4 "$IPV4" \
+        --param file "$IMAGE" >> result/results/result.txt
 
-    kill -SIGINT "$METER_PID"
+      kill -SIGINT "$METER_PID"
 
-    echo -e "$i"
+      echo -e "$i"
 
-    sleep 2
-      
+      sleep 4
+    done
+
+    sleep 4
+
   done
 
 done
