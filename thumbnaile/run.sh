b46d88064bd13adc1c38eb4b5b26a014765e6220
donaldonana <onanadonald@gmail.com>
2025-03-24 09:54:40 +0100

    new new setup

--- thumbnaile/run.sh
+++ thumbnaile/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-IPV4="172.20.20.77"
+IPV4="172.20.20.80"
+
 LIBRARY=("pillow" "wand" "pygame" "opencv")
 IMAGES=("100Kb.JPEG" "500b.JPEG" "1Mb.JPEG" "15Mb.JPEG" "256Kb.JPEG")
 
@@ -12,11 +13,10 @@ for IMAGE in "${IMAGES[@]}"; do
     echo -e "$LIB"
     ENERGY_FILE="result/energy/$IMAGE/$LIB$IMAGE.txt"  
 
-    for (( i = 1; i <= 100; i++ )); do
-      # Launch cpu-energy-meter in background and save its PID
-      cpu-energy-meter -r >> "$ENERGY_FILE" &
+    for (( i = 1; i <= 100; i++ )); do  
+      cpu-energy-meter -r >> "$ENERGY_FILE" &     # Launch cpu-energy-meter in background and save its PID
       METER_PID=$!
-
+      
       wsk action invoke thumb -r \
         --param bib  "$LIB" \
         --param ipv4 "$IPV4" \
@@ -24,11 +24,11 @@ for IMAGE in "${IMAGES[@]}"; do
 
       kill -SIGINT "$METER_PID"
       echo -e "$i"
-      sleep 4
+      sleep 2
 
     done
 
-    sleep 4
+    sleep 2
     
   done
 
